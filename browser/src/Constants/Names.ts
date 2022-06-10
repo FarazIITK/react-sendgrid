1397554b11ada42d7a61f3c9e9b8e8d9096f5fef
fhusain01 <fhusain@adaptready.com>
2022-06-11 03:08:00 +0530

    Added some constants for the project

--- browser/src/Constants/Names.ts
+++ browser/src/Constants/Names.ts
@@ -1,1 +1,12 @@
 export const companyName = '4 Korners Design Studio';
+export const ownerName = 'Vibhor Agarwal';
+export const ownerContactNumber = '+91 8755508044';
+export const companyAddress = {
+  plotNumber: '129',
+  locality: 'Balwant Singh Marg',
+  area: 'Civil Lines',
+  city: 'Bareilly',
+  state: 'Uttar Pradesh',
+  pinCode: '243001',
+  landmark: 'Dainik Jagran Press'
+};
