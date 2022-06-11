7e40f628a9b35de5996639dbfc90df3492f5cc2a
fhusain01 <fhusain@adaptready.com>
2022-06-12 00:23:35 +0530

    Setup router for the app

--- browser/src/App.tsx
+++ browser/src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './Styles/App.scss';
 import NavBar from './NavBar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home';
+import Services from './Components/Services';
+import Projects from './Components/Projects';
+import Contact from './Components/Contact';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
--- browser/src/NavBar.tsx
+++ browser/src/NavBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { companyName } from './Constants/Names';
 import companyLogo from './assets/company-logo.svg';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   // Constants used in the component
@@ -24,14 +25,26 @@ const NavBar = () => {
   return (
     <div className="nav-bar">
       <div className="nav-bar-left">
-        <img src={companyLogo} alt="company-logo" className="company-logo" />
-        <div className="company-name">{renderCompanyName()}</div>
+        <Link to="/">
+          <img src={companyLogo} alt="company-logo" className="company-logo" />
+        </Link>
+        <Link to="/">
+          <div className="company-name">{renderCompanyName()}</div>
+        </Link>
       </div>
       <div className="nav-bar-right">
-        <li>{navBarHomeLabel}</li>
-        <li>{navBarOurServicesLabel}</li>
-        <li>{navBarProjectsLabel}</li>
-        <li>{navBarContactLabel}</li>
+        <Link to="/">
+          <li>{navBarHomeLabel}</li>
+        </Link>
+        <Link to="/services">
+          <li>{navBarOurServicesLabel}</li>
+        </Link>
+        <Link to="/projects">
+          <li>{navBarProjectsLabel}</li>
+        </Link>
+        <Link to="/contact">
+          <li>{navBarContactLabel}</li>
+        </Link>
       </div>
     </div>
   );
