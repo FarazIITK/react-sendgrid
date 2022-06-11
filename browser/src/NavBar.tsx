b148f2d0e622544a7c1b2439819e78cbf1c83ed9
fhusain01 <fhusain@adaptready.com>
2022-06-11 16:40:44 +0530

    Added nav bar logo and styling

--- browser/src/NavBar.tsx
+++ browser/src/NavBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import './Styles/NavBar.scss';
 import { companyName } from './Constants/Names';
+import companyLogo from './assets/company-logo.svg';
 
 const NavBar = () => {
   // Constants used in the component
@@ -8,9 +8,25 @@ const NavBar = () => {
   const navBarOurServicesLabel = 'Services';
   const navBarProjectsLabel = 'Projects';
   const navBarContactLabel = 'Contact';
+
+  //   Function to render the comapnay name
+  const renderCompanyName = () => {
+    const letters = companyName.split(' ');
+    const companyNameFirstHalf = letters.slice(0, 2).join(' ');
+    const companyNameSecondHalf = letters.slice(2, 4).join(' ');
+    return (
+      <React.Fragment>
+        <h1>{companyNameFirstHalf}</h1>
+        <h1>{companyNameSecondHalf}</h1>
+      </React.Fragment>
+    );
+  };
   return (
     <div className="nav-bar">
-      <div className="nav-bar-left">{companyName}</div>
+      <div className="nav-bar-left">
+        <img src={companyLogo} alt="company-logo" className="company-logo" />
+        <div className="company-name">{renderCompanyName()}</div>
+      </div>
       <div className="nav-bar-right">
         <li>{navBarHomeLabel}</li>
         <li>{navBarOurServicesLabel}</li>
