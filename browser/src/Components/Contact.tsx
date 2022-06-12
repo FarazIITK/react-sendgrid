91cd008112922bbf2fc0991b8cae4081d08795c0
fhusain01 <fhusain@adaptready.com>
2022-06-12 23:56:45 +0530

    Added image and divider

--- browser/src/Components/Contact.tsx
+++ browser/src/Components/Contact.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import contactPageTopImage from '../assets/contact-page-top-image.jpg';
 
 const Contact = () => {
+  // const pageHeaderText = 'Contact Us';
   return (
-    <div>
-      <h1 className="contact">Contact</h1>
+    <div className="contact">
+      {/* <h1 className="contact-heading">{pageHeaderText}</h1> */}
+      <div className="contact-page-top-image">
+        <img
+          src={contactPageTopImage}
+          alt="Contact Page Top"
+          className="contact-page-top-image"
+        />
+      </div>
+      <div className="separator"></div>
     </div>
   );
 };
